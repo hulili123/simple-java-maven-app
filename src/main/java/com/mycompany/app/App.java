4029910fd2a7e7a049bc2d0e87d870339597ae48
hulili123 <129164940+hulili123@users.noreply.github.com>
2023-03-29 10:56:21 +0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,18 +1,31 @@
 package com.mycompany.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  */
+
+@RestController
+@SpringBootApplication
 public class App {
 
     private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
-    public static void main(String[] args) {
-        System.out.println(MESSAGE);
+    @GetMapping("/")
+    public String index() {
+        return "Hello Spring Boot";
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
     public String getMessage() {
         return MESSAGE;
     }
